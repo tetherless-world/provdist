12c1791974277217bbcd2fa4c2ce2ba9cd6ca92f
Benno <leeb5@rpi.edu>
2017-07-09 22:55:28 -0400

    Added json to the change log.  Extracted it to verSgSs.ttl

--- MBVL/vamps.py
+++ MBVL/vamps.py
@@ -24,7 +24,9 @@ f2 = "silva_spingo.xls"
 def gen_log(f1, f2):
 	rel1 = load_data(f1)
 	rel2 = load_data(f2)
-	fchange = "Change.%s.%s.html"%(f1.split('.')[0], f2.split('.')[0])
+	f1name = f1.split('.')[0]
+	f2name = f2.split('.')[0]
+	fchange = "Change.%s.%s.html"%(f1name, f2name)
 	changelog = open(fchange, 'w')
 
 	changelog.write('''<html>
@@ -55,12 +57,33 @@ def gen_log(f1, f2):
 	json.dump([json1,json2], changelog, indent=4, sort_keys=True)
 	changelog.write('''
       </script>
-    <h2>
+    </h2>
 ''')
 
 	for i in rel1.keys():
+		i_id = i.replace(" ", "_")
+
 		changelog.write('''
     <div about="%s%s">
+      <script type="application/ld+json">
+'''%(host, i))
+		Att1_json = {
+			"@context":context,
+			"@type":["vo:Version", "vo:Attribute"],
+			"@id":host+f1name+'-'+i_id,
+			"label":i,
+			"@reverse" :    { "hasAttribute" : "Version1" }
+		}
+		Att2_json = {
+			"@context":context,
+			"@type":["vo:Version", "vo:Attribute"],
+			"@id":host+f2name+'-'+i_id,
+			"label":i,
+			"@reverse" :    { "hasAttribute" : "Version2" }
+		}
+		json.dump([Att1_json, Att2_json], changelog, indent=4, sort_keys=True)
+		changelog.write('''
+      </script>
       <span style="font-weight:bold" property="http://www.w3.org/2000/01/rdf-schema#label">%s</span>
       <table>
         <tr>
@@ -69,38 +92,109 @@ def gen_log(f1, f2):
           <th>Count 1</th>
           <th>Count 2</th>
         </tr>\n
-'''%(host, i, i))
+'''%(i))
 		print i
 		for j in set(rel1[i].keys())-set(rel2[i].keys()):
+			j_id = j.replace(" ", "_")
+
 			print " removed", j
 			changelog.write('''
-        <tr>
+        <tr id=%sInvalidateChange%s>
           <td>---</td>
           <td>%s</td>
           <td>%i</td>
+          <script type="application/ld+json">
+'''%(host, j_id, j, rel1[i][j]))
+			json1 = {
+				"@context":context,
+				"@type":"vo:Attribute" ,
+				"@id":"".join([host, f1name, "-", i_id, '-',  j_id]) ,
+				"label":j ,
+				"undergoes":"".join([host, "InvalidateChange", j_id]) ,
+				"@reverse" :    { "hasAttribute" : Att1_json["@id"] }
+			}
+			json2 = {
+				"@context":context,
+				"@type":"vo:InvalidateChange" ,
+				"@id": json1["undergoes"] ,
+				"invalidatedBy" :   Att2_json["@id"],
+			}
+			json.dump([json1,json2], changelog, indent=4, sort_keys=True)
+			changelog.write('''
+          </script>
         </tr>
-'''%(j, rel1[i][j]))
+''')
 		for j in set(rel1[i].keys())&set(rel2[i].keys()):
+			j_id = j.replace(" ", "_")
+
 			if rel1[i][j] != rel2[i][j]:
 				print " modify", j, rel1[i][j], rel2[i][j]
 				changelog.write('''
-        <tr>
+        <tr id=%sModifyChange%s>
           <td>>>></td>
           <td>%s</td>
           <td>%i</td>
           <td>%i</td>
+          <script type="application/ld+json">
+'''%(host, j, j, rel1[i][j], rel2[i][j]))
+				json1 = {
+					"@context":context,
+					"@type":"vo:Attribute" ,
+					"@id":"".join([host, f1name, '-', i_id, '-', j_id]) ,
+					"label":j ,
+					"undergoes":"".join([host, "ModifyChange", j_id]) ,
+					"@reverse" :    { "hasAttribute" : Att1_json["@id"] }
+				}
+				json2 = {
+					"@context":context,
+					"@type":"vo:ModifyChange",
+					"@id":json1["undergoes"] ,
+					"resultsIn":"".join([host, f2name, '-', i_id, '-', j_id])
+				}
+				json3 = {
+					"@context":context,
+					"@type":"vo:Attribute" ,
+					"@id":json2["resultsIn"] ,
+					"label":j ,
+					"@reverse" :    { "hasAttribute" : Att2_json["@id"] }
+				}
+				json.dump([json1, json2, json3], changelog, indent=4, sort_keys=True)
+
+				changelog.write('''
+          </script>
         </tr>
-'''%(j, rel1[i][j], rel2[i][j]))
+''')
 		for j in set(rel2[i].keys())-set(rel1[i].keys()):
+			j_id = j.replace(" ", "_")
+
 			print " added", j
 			changelog.write('''
-        <tr>
+        <tr id=%sAddChange%s>
           <td>+++</td>
           <td>%s</td>
           <td></td>
           <td>%i</td>
+          <script type="application/ld+json">
+'''%(host, j_id, j, rel2[i][j]))
+			json1 = {
+				"@context":context,
+				"@type":"vo:AddChange" ,
+				"@id": "".join([host, "AddChange", j_id]) ,
+				"resultsIn" :   "".join([ host, f2name, '-', i_id, '-', j_id]),
+				"@reverse"  :   { "absentFrom": Att1_json["@id"] }
+			}
+			json2 = {
+				"@context":context,
+				"@type":"vo:Attribute" ,
+				"@id":json1["resultsIn"] ,
+				"label":j ,
+				"@reverse" :    { "hasAttribute" : Att2_json["@id"] }
+			}
+			json.dump([json1, json2], changelog, indent=4, sort_keys=True)
+			changelog.write('''
+          </script>
         </tr>
-'''%(j, rel2[i][j]))
+''')
 		changelog.write('''
       </table>
     </div>
