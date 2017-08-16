ee1c652e70df58ed79b2b9c3e08c604fd4193481
Benno <leeb5@rpi.edu>
2017-08-16 16:59:56 -0400

    Produced the comparisons for the other combinations

--- MBVL/raw_map.py
+++ MBVL/raw_map.py
@@ -81,26 +81,70 @@ def file_parse(f_name):
 	return results
 
 def get_output(tax1, alg1, tax2, alg2):
-	if alg1 != alg2:
-		for j in range(7):
-			if j > 4:
-				ss_tax_index = (2*j)+2
-			else:
-				ss_tax_index = 2*j
+	if alg1 == 'silva_spingo':
+		t1 = [x for x in tax1 if not is_number(x)]
+		del t1[5]
+	elif alg1 == 'rdp_spingo':
+		t1 = [x for x in tax1 if not is_number(x)]
+		print t1
+		del t1[1]
+		t1[0:0] = ['Pass', 'Pass', 'Pass', 'Pass']
+	else:
+		t1 = tax1
 
-			if j >= len(sg[i].tax):
-				if tax1[ss_tax_index] != 'AMBIGUOUS':
-					return "\t".join([ '---', rank[j], str([x for x in tax1[ss_tax_index:] if not is_number(x)])])
+	if alg2 == 'silva_spingo':
+		t2 = [x for x in tax2 if not is_number(x)]
+		del t2[5]
+	elif alg2 == 'rdp_spingo':
+		t2 = [x for x in tax2 if not is_number(x)]
+		del t2[1]
+		t2[0:0] = ['Pass', 'Pass', 'Pass', 'Pass']
+	else:
+		t2 = tax2
+
+	if alg1.split('_')[1] != alg2.split('_')[1]:
+		for j in range(7):
+			if t1[j] == 'Pass':
+				continue
+			if j >= len(t2):
+				if t1[j] != 'AMBIGUOUS':
+					return "\t".join([ '---', rank[j], str(t1[j:])])
 			elif j == 6:
-				if tax1[ss_tax_index] == 'AMBIGUOUS':
-					return "\t".join(['+++', rank[j], str(tax2[j:])])
-				elif ss[i].tax[ss_tax_index] != '_'.join(tax2[5:7]):
-					return "\t".join([ '>>>', rank[j], tax1[ss_tax_index], str(tax2[5:])])
-			elif tax1[ss_tax_index] == 'AMBIGUOUS':
-				return "\t".join([ '+++', rank[j], str(tax2[j:])])
-			elif tax1[ss_tax_index] != tax2[j]:
-				return "\t".join([ '>>>', rank[j], str(tax1[ss_tax_index]), str(tax2[j:])])
+				if t1[j] == 'AMBIGUOUS':
+					return "\t".join(['+++', rank[j], str(t2[j:])])
+				elif t1[j] != '_'.join(t2[5:7]):
+					return "\t".join([ '>>>', rank[j], t1[j], str(tax2[5:])])
+			elif t1[j] == 'AMBIGUOUS':
+				return "\t".join([ '+++', rank[j], str(t2[j:])])
+			elif t1[j] != t2[j]:
+				return "\t".join([ '>>>', rank[j], str(t1[j]), str(t2[j:])])
 		return None
+	elif alg1.split('_')[1] == 'spingo':
+		for j in range(7):
+			if t1[j] == 'Pass' or t2[j] == 'Pass':
+				continue
+			if t1[j] == 'AMBIGUOUS':
+				if t2[j] == 'AMBIGUOUS':
+					return None
+				else:
+					return "\t".join(['+++', rank[j], str(t2[j:])])
+			elif t2[j] == 'AMBIGUOUS':
+				return "\t".join(['---', rank[j], str(t1[j:])])
+			elif t1[j] != t2[j]:
+				return "\t".join(['>>>', rank[j], str(t1[j]), str(t2[j])])
+		return None
+	elif alg1.split('_')[1] == 'gast':
+		if len(t1) > len(t2):
+			return "\t".join(['---', rank[len(t2)], str(t1[len(t2):])])
+		elif len(t1) < len(t2):
+			return "\t".join(['+++', rank[len(t1)], str(t2[len(t1):])])
+		else:
+			for j in range(len(t1)):
+				if t1[j] != t2[j]:
+					return "\t".join(['>>>', rank[j], str(t1[j]), str(t2[j])])
+		return None
+	else:
+		return -1
 
 mock = ['Acinetobacter baumannii',
 	'Actinomyces odontolyticus',
@@ -124,16 +168,18 @@ mock = ['Acinetobacter baumannii',
 	'Streptococcus mutans',
 	'Streptococcus pneumoniae']
 
-rank = ['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']
+rank = ['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species', 'Strain']
 
 if __name__ == "__main__":
 	fList = ['silva_spingo', 'silva_gast', 'rdp_spingo', 'rdp_gast']
-	ss = file_parse(fList[0])
-	sg = file_parse(fList[1])
+	f1_name = fList[1]
+	f2_name = fList[3]
+	f1 = file_parse(f1_name)
+	f2 = file_parse(f2_name)
 
-	out = file('silva_spingo_gast.txt', 'w')
-	for i in sorted(ss.keys()):
-		x = get_output(ss[i].tax, 'spingo', sg[i].tax, 'gast')
+	out = file('silva_rdp_gast.txt', 'w')
+	for i in sorted(f1.keys()):
+		x = get_output(f1[i].tax, f1_name, f2[i].tax, f2_name)
 		if not x == None:
 			out.write("\t".join([i,x])+"\n")
 	out.close()
