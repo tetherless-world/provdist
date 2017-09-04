0111bc279bdfb924174676ae4348783d11fd484b
Benno <leeb5@rpi.edu>
2017-09-04 17:15:32 -0400

    Raw map should now be able to generally handle the 4 different comparisons without needing edits

--- MBVL/raw_map.py
+++ MBVL/raw_map.py
@@ -1,3 +1,4 @@
+import urllib
 
 class entry:
 	query = None
@@ -23,12 +24,19 @@ def file_parse(f_name):
 	f = open(f_name, 'r')
 	found = {}
 	for i in mock:
-		found[i] = 0
+		found[i] = [[],[],0]
+	ambiguous = []
 	fp = []
+	fn_count = 0
 	fn = []
 	results = {}
 	total = 0
 	if f_name.split('_')[1] == 'spingo':
+		if f_name == "silva_spingo":
+			family[4] = 'Clostridiaceae_1'
+		else:
+			family[2] = 'Bacillaceae 1'
+			family[4] = 'Clostridiaceae 1'
 		for line in f:
 			x = line.split('\t')
 			entry_id = x[0].split('|')[0]
@@ -53,27 +61,30 @@ def file_parse(f_name):
 			results[entry_id].tax = clean_tax
 
 			if clean_tax[4] == 'AMBIGUOUS':
-				for i in found.keys():
-					found[i] += 0.125
+				fn_count += 1
+				ambiguous.append(entry_id)
 			elif not clean_tax[4] in family:
 				fp.append((entry_id, 'family'))
 			elif clean_tax[5] == 'AMBIGUOUS':
 				for i in range(len(family)):
 					if clean_tax[4] == family[i]:
-						found[mock[i]] += 0.25
+						found[mock[i]][0].append(entry_id)
 			elif not clean_tax[5] in genus:
 				fp.append((entry_id, 'genus'))
 			elif clean_tax[6] == 'AMBIGUOUS':
 				for i in range(len(genus)):
 					if clean_tax[5] == genus[i]:
-						found[mock[i]] += 0.5
+						found[mock[i]][1].append(entry_id)
 			elif not ' '.join(clean_tax[6].split('_')) in mock:
 				fp.append((entry_id, 'species'))
 			else:
-				found[' '.join(clean_tax[6].split('_'))] += 1
+				found[' '.join(clean_tax[6].split('_'))][2] += 1
 
 	elif f_name.split('_')[1] == 'gast':
 		skip = False
+		if f_name == "rdp_gast":
+			family[2] = 'Bacillaceae 1'
+			family[4] = 'Clostridiaceae 1'
 		for line in f:
 			if not skip:
 				skip = True
@@ -90,38 +101,71 @@ def file_parse(f_name):
 			temp_tax = results[entry_id].tax
 
 			if len(temp_tax) < 5:
-				for i in found.keys():
-					found[i] += 0.125
+				fn_count += 1
+				ambiguous.append(entry_id)
 			elif not temp_tax[4] in family:
 				fp.append((entry_id, 'family'))
 			elif len(temp_tax) < 6:
 				for i in range(len(family)):
 					if temp_tax[4] == family[i]:
-						found[mock[i]] += 0.25
+						found[mock[i]][0].append(entry_id)
 			elif not temp_tax[5] in genus:
 				fp.append((entry_id, 'genus'))
 			elif len(temp_tax) < 7:
 				for i in range(len(genus)):
 					if temp_tax[5] == genus[i]:
-						found[mock[i]] += 0.5
+						found[mock[i]][1].append(entry_id)
 			elif not ' '.join(results[entry_id].tax[5:7]) in mock:
 				fp.append((entry_id, 'species'))
 			else:
-				found[' '.join(results[entry_id].tax[5:7])] += 1#results[entry_id].freq
+				found[' '.join(results[entry_id].tax[5:7])][2] += 1
 
 	fp_file = open("fp_"+f_name+".txt", 'w')
 	for fid, frank in fp:
 		fp_file.write("\t".join([fid, str(results[fid].freq), frank, str(results[fid].tax)])+"\n")
 	fp_file.close()
+
+	for i in found.keys():
+		if sum([len(found[i][0]), len(found[i][1]), found[i][2]]) == 0:
+			fn.append(i)
+	print fn
+	ttl_output(f_name, fp, fn, found, results)
 	total = len(results.keys())
-	print "False Positives: ", len(fp)
-	print "Total: ", len(results.keys())
-	coverage = sum([found[i] for i in found.keys()])
-	print "Coverage: ", coverage, "(",float(coverage)/total*100, "%)"
+	print "\t".join(["False Positives:", str(len(fp))])
+	print "\t".join(["Ambiguous:", str(len(ambiguous))])
+	print "\t".join(["Total:", str(len(results.keys()))])
+	coverage = sum([len(found[i][0])+len(found[i][1])+found[i][2] for i in found.keys()])
+	print "\t".join(["Coverage:", str(coverage)])
+	print "\t".join(["Percentage:", str(float(coverage)/total*100)])
 	for k in sorted(mock):
-		print k, " :\t", found[k]
+		print "\t".join([k+":", str(len(found[k][0])), str(len(found[k][1])), str(found[k][2])])
 	
 	return results
+
+def ttl_output(f_name, false_positive, false_negative, found, results):
+	fp_file = open("fp_"+f_name+".ttl", 'w')
+	fp_file.write("""@prefix vo: <http://orion.tw.rpi.edu/~blee/VersionOntology.owl#> .
+@prefix mbvl: <http://example.com/MBVL/> .
+@prefix next: <http://example.com/MBVL/%s> .
+@prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix xml: <http://www.w3.org/XML/1998/namespace> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+
+<http://example.com/MBVL/db> a vo:Version .
+<http://example.com/MBVL/%s> a vo:Version .\n"""%(f_name, f_name))
+	for i in range(len(false_positive)):
+		fid, j = false_positive[i]
+		clean_fid = urllib.quote(fid.split()[0])
+		if results[fid].freq > 10:
+			fp_file.write("""mbvl:db vo:absentFrom mbvl:AddChange%i .
+next:%s a vo:Attribute .
+mbvl:%s vo:hasAttribute next:%s .
+mbvl:AddChange%i a vo:AddChange ;
+	vo:resultsIn next:%s .\n
+"""%(i, clean_fid, f_name, clean_fid, i, clean_fid))
+	fp_file.close()
 
 def get_output(tax1, alg1, tax2, alg2):
 	if alg1 == 'silva_spingo':
@@ -215,9 +259,9 @@ genus = [x.split()[0] for x in mock]
 
 family = ['Moraxellaceae',
 	  'Actinomycetaceae',
-	  'Bacillaceae 1',
+	  'Bacillaceae',
 	  'Bacteroidaceae',
-	  'Clostridiaceae 1',
+	  'Clostridiaceae',
 	  'Deinococcaceae',
 	  'Enterococcaceae',
 	  'Enterobacteriaceae',
@@ -239,11 +283,13 @@ rank = ['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species', 'St
 
 if __name__ == "__main__":
 	fList = ['silva_spingo', 'silva_gast', 'rdp_spingo', 'rdp_gast']
-	f1_name = fList[3]
+	#f1_name = fList[1]
 	#f2_name = fList[3]
-	f1 = file_parse(f1_name)
+	#f1 = file_parse(f1_name)
 	#f2 = file_parse(f2_name)
 
+	for i in fList:
+		f1 = file_parse(i)
 	#out = file('silva_rdp_gast.txt', 'w')
 	#for i in sorted(f1.keys()):
 	#	x = get_output(f1[i].tax, f1_name, f2[i].tax, f2_name)
